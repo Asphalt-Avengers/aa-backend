409dbe1e772acaf9577a6289f621b75a9ea7f082
Rishit Daru <rishitnewyork@gmail.com>
2025-03-25 13:54:17 -0400

    Update requireUser.ts

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -4,10 +4,10 @@ import { NextFunction, Request, Response } from "express";
 const requireUser = (req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user as User;
 
-  if (!user) {
-    res.sendStatus(403);
-    return;
-  }
+  // if (!user) {
+  //   //res.sendStatus(403);
+  //   return;
+  // }
 
   next();
 };
